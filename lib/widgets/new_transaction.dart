91c009de96ceb9e7c2203f592326379cade549b6
triyono <yono.tri@gmail.com>
2020-03-04 06:07:59 +0700

    tambah navigator pop

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -22,6 +22,7 @@ class _NewTransactionState extends State<NewTransaction> {
       return;
     }
     widget.addTx(enteredTitle, enteredAmount);
+    Navigator.of(context).pop();
   }
 
   @override
