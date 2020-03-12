3a1c1b442503800c03dbf136deeb0c1b81ed55ec
triyono <yono.tri@gmail.com>
2020-03-13 06:01:32 +0700

    fix height media query

--- lib/main.dart
+++ lib/main.dart
@@ -110,30 +110,42 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    final appBar = AppBar(
+      title: Text(
+        'Expenses App',
+      ),
+      actions: <Widget>[
+        IconButton(
+          icon: Icon(Icons.add),
+          onPressed: () => startAddnewTransaction(context),
+        ),
+      ],
+    );
     return Scaffold(
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       floatingActionButton: FloatingActionButton(
         onPressed: () => startAddnewTransaction(context),
         child: Icon(Icons.add),
       ),
-      appBar: AppBar(
-        title: Text(
-          'Expenses App',
-        ),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.add),
-            onPressed: () => startAddnewTransaction(context),
-          ),
-        ],
-      ),
+      appBar: appBar,
       body: SingleChildScrollView(
         child: Column(
           children: <Widget>[
-            Chart(_recentTransaction),
-            TransactionList(
-              transaction: _userTransaction,
-              deleteTx: _deleteTransaction,
+            Container(
+                height: (MediaQuery.of(context).size.height -
+                        appBar.preferredSize.height -
+                        MediaQuery.of(context).padding.top) *
+                    0.4,
+                child: Chart(_recentTransaction)),
+            Container(
+              height: (MediaQuery.of(context).size.height -
+                      appBar.preferredSize.height -
+                      MediaQuery.of(context).padding.top) *
+                  0.6,
+              child: TransactionList(
+                transaction: _userTransaction,
+                deleteTx: _deleteTransaction,
+              ),
             )
           ],
         ),
--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -11,51 +11,48 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 450,
-      child: transaction.isEmpty
-          ? Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Text('Belum ada transaksi'),
-                  Icon(
-                    Icons.remove_shopping_cart,
-                    size: 30,
-                  )
-                ],
-              ),
-            )
-          : ListView.builder(
-              itemCount: transaction.length,
-              itemBuilder: (contxt, index) {
-                return Card(
-                  margin: EdgeInsets.symmetric(horizontal: 5, vertical: 8),
-                  child: ListTile(
-                    leading: CircleAvatar(
-                      radius: 30,
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: FittedBox(
-                            child: Text('\$${transaction[index].amount}')),
-                      ),
-                    ),
-                    title: Text(
-                      transaction[index].title,
-                      style: Theme.of(context).textTheme.title,
-                    ),
-                    subtitle: Text(
-                      DateFormat.yMMMMd().format(transaction[index].date),
-                    ),
-                    trailing: IconButton(
-                      icon: Icon(Icons.delete),
-                      onPressed: () => deleteTx(transaction[index].id),
-                      color: ThemeData().errorColor,
+    return transaction.isEmpty
+        ? Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text('Belum ada transaksi'),
+                Icon(
+                  Icons.remove_shopping_cart,
+                  size: 30,
+                )
+              ],
+            ),
+          )
+        : ListView.builder(
+            itemCount: transaction.length,
+            itemBuilder: (contxt, index) {
+              return Card(
+                margin: EdgeInsets.symmetric(horizontal: 5, vertical: 8),
+                child: ListTile(
+                  leading: CircleAvatar(
+                    radius: 30,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: FittedBox(
+                          child: Text('\$${transaction[index].amount}')),
                     ),
                   ),
-                );
-              },
-            ),
-    );
+                  title: Text(
+                    transaction[index].title,
+                    style: Theme.of(context).textTheme.title,
+                  ),
+                  subtitle: Text(
+                    DateFormat.yMMMMd().format(transaction[index].date),
+                  ),
+                  trailing: IconButton(
+                    icon: Icon(Icons.delete),
+                    onPressed: () => deleteTx(transaction[index].id),
+                    color: ThemeData().errorColor,
+                  ),
+                ),
+              );
+            },
+          );
   }
 }
