431785d92fcbcf200185f5e64588f0fac0a0710c
triyono <yono.tri@gmail.com>
2020-03-13 21:17:51 +0700

    add layout builder

--- lib/main.dart
+++ lib/main.dart
@@ -135,13 +135,13 @@ class _MyHomePageState extends State<MyHomePage> {
                 height: (MediaQuery.of(context).size.height -
                         appBar.preferredSize.height -
                         MediaQuery.of(context).padding.top) *
-                    0.4,
+                    0.3,
                 child: Chart(_recentTransaction)),
             Container(
               height: (MediaQuery.of(context).size.height -
                       appBar.preferredSize.height -
                       MediaQuery.of(context).padding.top) *
-                  0.6,
+                  0.7,
               child: TransactionList(
                 transaction: _userTransaction,
                 deleteTx: _deleteTransaction,
--- lib/widgets/chart_bar.dart
+++ lib/widgets/chart_bar.dart
@@ -8,44 +8,50 @@ class ChartBar extends StatelessWidget {
   ChartBar(this.label, this.spendidngAmount, this.spendPctOfTotal);
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Container(
-            height: 20,
-            child: FittedBox(
-                child: Text('\$${spendidngAmount.toStringAsFixed(0)}'))),
-        SizedBox(
-          height: 4,
-        ),
-        Container(
-          height: 60,
-          width: 10,
-          child: Stack(
-            children: <Widget>[
-              Container(
-                decoration: BoxDecoration(
-                  border: Border.all(color: Colors.grey, width: 1),
-                  color: Color.fromRGBO(220, 220, 220, 1),
-                  borderRadius: BorderRadius.circular(20),
-                ),
-              ),
-              FractionallySizedBox(
-                heightFactor: spendPctOfTotal,
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Theme.of(context).primaryColor,
-                    borderRadius: BorderRadius.circular(10),
+    return LayoutBuilder(
+      builder: (ctx, constrains) {
+        return Column(
+          children: <Widget>[
+            Container(
+                height: constrains.maxHeight * 0.15,
+                child: FittedBox(
+                    child: Text('\$${spendidngAmount.toStringAsFixed(0)}'))),
+            SizedBox(
+              height: constrains.maxHeight * 0.05,
+            ),
+            Container(
+              height: constrains.maxHeight * 0.6,
+              width: 10,
+              child: Stack(
+                children: <Widget>[
+                  Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.grey, width: 1),
+                      color: Color.fromRGBO(220, 220, 220, 1),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
                   ),
-                ),
+                  FractionallySizedBox(
+                    heightFactor: spendPctOfTotal,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Theme.of(context).primaryColor,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            ],
-          ),
-        ),
-        SizedBox(
-          height: 4,
-        ),
-        Text(label),
-      ],
+            ),
+            SizedBox(
+              height: constrains.maxHeight * 0.05,
+            ),
+            Container(
+                height: constrains.maxHeight * 0.15,
+                child: FittedBox(child: Text(label))),
+          ],
+        );
+      },
     );
   }
 }
